48d8b02ac622ea1b65b5d6410f78596ae185e262
Диана Кабдулова <ms.kabdulova@gmail.com>
2023-05-02 23:43:57 +0300

    second

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = '/back_static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'back_static')
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
