98ff4b0a231fabb297293561d51135bad082c1e2
Диана Кабдулова <ms.kabdulova@gmail.com>
2023-05-09 15:35:05 +0300

    second

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -173,7 +173,7 @@ class RecipeListSerializer(serializers.ModelSerializer):
     """Получение списка рецептов/рецепта."""
 
     author = CustomUserSerializer(read_only=True)
-    tags = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    tags = serializers.SerializerMethodField()
     image = Base64ImageField()
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
@@ -183,6 +183,12 @@ class RecipeListSerializer(serializers.ModelSerializer):
         """Возвращает отдельный сериализатор."""
         return IngredientRecipeSerializer(
             IngredientRecipe.objects.filter(recipe__id=obj.id).all(), many=True
+        ).data
+
+    def get_tags(self, obj):
+        """Возвращает отдельный сериализатор."""
+        return TagSerializer(
+            Tag.objects.filter(recipe__id=obj.id).all(), many=True
         ).data
 
     def get_is_favorited(self, obj):
@@ -264,6 +270,12 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
         super().update(instance, validated_data)
         instance.save()
         return instance
+
+    def get_tags(self, obj) :
+        """Возвращает отдельный сериализатор."""
+        return TagSerializer(
+            Tag.objects.filter(recipe__id=obj.id).all(), many=True
+        ).data
 
     def get_is_favorited(self, obj):
         request = self.context.get('request')
