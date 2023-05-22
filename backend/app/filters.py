980962de7fec601bfbef90249ae92b997256549d
Диана Кабдулова <ms.kabdulova@gmail.com>
2023-05-22 20:13:44 +0300

    second

--- backend/app/filters.py
+++ backend/app/filters.py
@@ -42,5 +42,5 @@ class RecipeFilter(FilterSet):
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(carts__user=self.request.user)
         return queryset
