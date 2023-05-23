b723b18af42af0733f52634d362886770bd564e0
Диана Кабдулова <ms.kabdulova@gmail.com>
2023-05-23 21:33:01 +0300

    second

--- backend/app/filters.py
+++ backend/app/filters.py
@@ -33,7 +33,7 @@ class RecipeFilter(FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags')
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
