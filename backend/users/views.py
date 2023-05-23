6a5c0f3488a1b5811586fabecdcc49ae9981afc4
Диана Кабдулова <ms.kabdulova@gmail.com>
2023-05-23 23:12:30 +0300

    second

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,6 +6,8 @@ from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 
 from .models import Subscribe, User
 from .serializers import CustomUserSerializer, SubscribeSerializer
