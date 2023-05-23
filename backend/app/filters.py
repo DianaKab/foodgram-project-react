ffbc8cc7c139280b1320c52bd282bb3839f92a32
Диана Кабдулова <ms.kabdulova@gmail.com>
2023-05-23 21:55:01 +0300

    second

--- backend/app/filters.py
+++ backend/app/filters.py
@@ -37,10 +37,10 @@ class RecipeFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(favorites__user=request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(carts__user=request.user)
+            return queryset.filter(carts__user=self.request.user)
         return queryset
